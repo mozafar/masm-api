b8780b3caa88936f17f976b80586f76a12781cab
m.gholami <mozafar.gholami@gmail.com>
2021-06-25 03:15:14 +0430

    Implement model factories

---
+++ database/factories/DeviceFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\App;
+use App\Models\Device;
+use App\Models\OS;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class DeviceFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Device::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $os = (OS::count() < 2) ? OS::factory()->create() : OS::all()->random();
+        return [
+            'u_id' => $this->faker->randomNumber(null, false),
+            'app_id' => App::factory()->create()->id,
+            'os_id' => $os->id,
+            'language' => $this->faker->randomElement(['EN', 'FA', 'AR', 'TR']),
+            'token' => hash('sha256', Str::random(40))
+        ];
+    }
+}
