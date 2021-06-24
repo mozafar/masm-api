100a6aa5d82ac206f6b8458906bf1e885de3256d
m.gholami <mozafar.gholami@gmail.com>
2021-06-25 03:13:07 +0430

    Add app table migration

---
+++ database/migrations/2021_06_24_143510_create_app_tables.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        Schema::create('apps', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('os_id');
+            $table->foreign('os_id')->references('id')->on('oses');
+            $table->string('username');
+            $table->string('password');
+            $table->unique(['id', 'os_id']);
+        });
+
+        Schema::create('devices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('u_id');
+            $table->unsignedBigInteger('app_id');
+            $table->foreign('app_id')->references('id')->on('apps');
+            $table->unsignedBigInteger('os_id');
+            $table->foreign('os_id')->references('id')->on('oses');
+            $table->string('language', 2);
+            $table->string('token', 64);
+        });
+
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->unsignedBigInteger('device_id');
+            $table->foreign('device_id')->references('id')->on('devices');
+            $table->unsignedBigInteger('app_id');
+            $table->foreign('app_id')->references('id')->on('apps');
+            $table->string('receipt');
+            $table->enum('status', ['pending', 'active', 'expired', 'cancelled']);
+            $table->timestamp('expires_at');
+            $table->primary(['device_id', 'app_id']);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+        Schema::dropIfExists('devices');
+        Schema::dropIfExists('apps');
+        Schema::dropIfExists('oses');
+    }
+}
